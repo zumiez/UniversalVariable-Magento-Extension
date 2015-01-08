ed75cfac5022ff3c50ff8c8a87b9ddb0104f9e31
zainengineer <zainengineer@gmail.com>
2015-01-08 18:39:42 +1030

    added fix for virtual order (giftcards only orders)
    added fix for virtual order (giftcards only orders)
    without the fix a fatal error will occur

--- app/code/community/QuBit/UniversalVariable/Model/Page/Observer.php
+++ app/code/community/QuBit/UniversalVariable/Model/Page/Observer.php
@@ -534,7 +534,9 @@ class QuBit_UniversalVariable_Model_Page_Observer {
       $transaction['shipping_method'] = $this->_extractShippingMethod($order);
 
       // Get addresses
-      if (method_exists($order,'getShippingAddress')) {
+      //checking for getShippingAddress() otherwise $order->getShippingAddress()->getId() will result in fatal error
+      //if order is all virtual i.e. only gift cards are purchased.
+      if (method_exists($order,'getShippingAddress') && $order->getShippingAddress()) {
         $shippingId        = $order->getShippingAddress()->getId();
         $shippingAddress   = $order->getShippingAddress();
         $transaction['delivery'] = $this->_getAddress($shippingAddress);
